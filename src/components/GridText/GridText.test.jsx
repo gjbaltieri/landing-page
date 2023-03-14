6862c1210082356596b60b7364c9efbf3ee91c08
Gustavo <gjbaltieri@gmail.com>
2023-03-14 16:09:54 -0300

    test: Grid Text component

--- src/components/GridText/GridText.test.jsx
+++ src/components/GridText/GridText.test.jsx
@@ -3,10 +3,11 @@ import '@testing-library/jest-dom';
 import { screen } from '@testing-library/react';
 import renderTheme from '../../style/renderTheme';
 import GridText from './GridText';
+import Mock from '../GridText/mock-GridText';
 
 describe('<GridText/>', () => {
   test('should render GridText with default values', () => {
-    renderTheme(<GridText>children</GridText>);
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+    const { container } = renderTheme(<GridText {...Mock} />);
+    expect(container).toMatchSnapshot();
   });
 });
